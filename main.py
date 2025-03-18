b3b79fe7afd8b4699f28d8b3493904b9209bb70b
eagle-tex <70228400+eagle-tex@users.noreply.github.com>
2025-03-18 17:56:35 +0100

    feat(main.py): update main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from functions import add, sub, div, mult
-from blue import greeting, helper
-from green.counting import count
-from blue.other.fourth import last_function
+from src.math_func import add, sub, div, mult
 
 x, y = 5, 4
 print(add(x, y))
 print(sub(x, y))
 print(mult(x, y))
 print(div(x, y))
-greeting.greet()
-helper.help_me()
-count(5)
-last_function()
